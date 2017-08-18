83011c5fd6dc4d8a10a6f03d3ecd223bda1ed002
Unknown <vishva.sudantha@gmail.com>
2017-08-18 11:35:55 +0530

    created service and employee entity

---
+++ app/Entities/EmployeeEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inova
+ * Date: 8/18/2017
+ * Time: 11:27 AM
+ */
+
+namespace App\Entities;
+
+
+class EmployeeEntity
+{
+
+}
---
+++ app/Entities/ServiceEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inova
+ * Date: 8/18/2017
+ * Time: 11:28 AM
+ */
+
+namespace App\Entities;
+
+
+class ServiceEntity
+{
+
+}
